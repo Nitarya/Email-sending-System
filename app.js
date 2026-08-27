da76dc149d57c8cc04bf350e0e775f385bd8fd8b
Nitya <niittss27@gmail.com>
2026-08-27 08:22:27 +0530

    UI Fixed

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const app = express();
 app.set('view engine', 'ejs');
 const port = 3001;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.post('/send-email', (req, res) => {
+    console.log(req.body);
+    res.send('I got the data. Check the console. Check the terminal');
 });
 
 app.listen(port, () => {
